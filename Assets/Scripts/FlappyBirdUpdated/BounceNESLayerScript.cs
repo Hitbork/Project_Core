0eea72645bfd05327361bdf9e2d62908d0270cd1
hitbork <hitbork@yandex.ru>
2024-05-23 13:48:07 +0300

    Created all directions for BounceNES tilemaps
    Also: made BounceNESLayerScript more readable

--- Assets/Scripts/FlappyBirdUpdated/BounceNESLayerScript.cs
+++ Assets/Scripts/FlappyBirdUpdated/BounceNESLayerScript.cs
@@ -6,40 +6,88 @@ using UnityEngine;
 public class BounceNESLayerScript : MonoBehaviour
 {
     public Rigidbody2D birdRigidBody;
+    private string layerName;
     
     private void Start()
     {
-        if (GameObject.FindGameObjectWithTag("Player")!= null)
-        {
-            birdRigidBody = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
-        } 
+        // Defining the name of each layer
+        layerName = this.gameObject.name;
+
+        // Trying to find rigidbody of player
+        SetRigidbody();
     }
 
+    private void SetRigidbody()
+    {
+        try
+        {
+            birdRigidBody = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+        }
+        // Catching the error because gameobject 
+        // tagged player may be set unactive
+        catch (Exception ex)
+        {
+            Debug.Log("BounceNESLayer couldn't find the bird");
+        }
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
-    {        
-        if (this.gameObject.name == "BounceNESRight")
+    {
+        Vector2Int tempVector = new Vector2Int(0, 0);
+
+        // Defining the direction of vector
+        switch (layerName)
         {
-            birdRigidBody.velocity = new Vector2(15, 0);
-            return;
+            // Direction: Upwards
+            case "BounceNESUp":
+                tempVector.y = 15;
+                break;
+
+            // Direction: Upwards-right
+            case "BounceNESUpRight":
+                tempVector.x = 15;
+                tempVector.y = 15;
+                break;
+
+            // Direction: Right
+            case "BounceNESRight":
+                tempVector.x = 15;
+                break;
+
+            // Direction: Downwards-right
+            case "BounceNESDownRight":
+                tempVector.x = 15;
+                tempVector.y = 15;
+                break;
+
+            // Direction: Downwards
+            case "BounceNESDown":
+                tempVector.y = -15;
+                break;
+
+            // Direction: Dowwards-left
+            case "BounceNESDownLeft":
+                tempVector.x = -15;
+                tempVector.y = -15;
+                break;
+
+            // Direction: Left
+            case "BounceNESLeft":
+                tempVector.x = -15;
+                break;
+
+            // Direction: Dowwards-left
+            case "BounceNESUpLeft":
+                tempVector.x = -15;
+                tempVector.y = 15;
+                break;
+
+            // Direction: undefined
+            default:
+                Debug.Log("Wrong name of layer");
+                break;
         }
 
-        if (this.gameObject.name == "BounceNESLeft")
-        {
-            birdRigidBody.velocity = new Vector2(-15, 0);
-            return;
-        }
-
-        if (this.gameObject.name == "BounceNESUp")
-        {
-            birdRigidBody.velocity = new Vector2(0, 15);
-            return;
-        }
-
-        if (this.gameObject.name == "BounceNESDown")
-        {
-            birdRigidBody.velocity = new Vector2(0, -15);
-            return;
-        }
+        birdRigidBody.velocity = tempVector;
     }
 }
