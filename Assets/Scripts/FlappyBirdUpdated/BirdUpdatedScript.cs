57e11e84970275d511e7e8fb3eee79aa3a52436d
hitbork <hitbork@yandex.ru>
2024-05-28 22:42:41 +0300

    Task5 not working code for showing

--- Assets/Scripts/FlappyBirdUpdated/BirdUpdatedScript.cs
+++ Assets/Scripts/FlappyBirdUpdated/BirdUpdatedScript.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class BirdUpdatedScript : MonoBehaviour
 {
     public Rigidbody2D myRigidBody2D;
-    public float speed = 5.0f;
+    public Transform transformOfBird;
+    public float multiplierToRotation = 2f, 
+        speed = 5.0f, 
+        multiplierToVelocity = 2f;
     public LogicScript logic;
-    private bool birdIsAlive = true;
+    private bool birdIsAlive = true, 
+        isRotatingBack = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,14 +23,50 @@ public class BirdUpdatedScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float angleOfBird = transformOfBird.rotation.z;
+        if (isRotatingBack)
+        {
+            if (angleOfBird != 0)
+            {
+                var trans = transformOfBird.rotation;
+                if (angleOfBird <= 180 && angleOfBird >= 0)
+                {
+                    if (angleOfBird <= multiplierToRotation)
+                    {
+                        angleOfBird = 0;
+                        isRotatingBack = false;
+                    }
+                    else
+                        angleOfBird -= multiplierToRotation * Time.deltaTime;
+                }
+                else
+                {
+                    if (angleOfBird >= multiplierToRotation * (-1))
+                    {
+                        angleOfBird = 0;
+                        isRotatingBack = false;
+                    }
+                    else
+                        angleOfBird += multiplierToRotation * Time.deltaTime;
+                }
+                trans.z = angleOfBird;
+                transformOfBird.rotation = trans;
+            }
+        }
+        else
+        {
+            if (angleOfBird >= 100 || angleOfBird <= 100)
+                isRotatingBack = true;
+        }
+
         if (myRigidBody2D.velocity.x > 5.0f && birdIsAlive)
         {
-            myRigidBody2D.velocity -= Vector2.left * 2f * Time.deltaTime;
+            myRigidBody2D.velocity -= Vector2.left * multiplierToVelocity * Time.deltaTime;
         }
 
         if (myRigidBody2D.velocity.x < 5.0f && birdIsAlive)
         {
-            myRigidBody2D.velocity += Vector2.right * 2f * Time.deltaTime;
+            myRigidBody2D.velocity += Vector2.right * multiplierToVelocity * Time.deltaTime;
         }
         
         if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
