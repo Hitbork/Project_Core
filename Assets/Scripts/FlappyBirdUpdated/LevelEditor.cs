112bb2e76174223978665e69ba74b46f2c80f740
hitbork <hitbork@yandex.ru>
2024-05-29 00:30:54 +0300

    Task6 moving and scaling options
    Created moving and scaling options by pressing buttons of keyboard

--- Assets/Scripts/FlappyBirdUpdated/LevelEditor.cs
+++ Assets/Scripts/FlappyBirdUpdated/LevelEditor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -45,6 +46,16 @@ public class LevelEditor : MonoBehaviour
     private void Update()
     {
         Vector3Int pos = currentTilemap.WorldToCell(cam.ScreenToWorldPoint(Input.mousePosition));
+
+        // Moving camera options
+        if (Input.GetKeyDown(KeyCode.D)) MovingCamera("right");
+        if (Input.GetKeyDown(KeyCode.A)) MovingCamera("left");
+        if (Input.GetKeyDown(KeyCode.W)) MovingCamera("up");
+        if (Input.GetKeyDown(KeyCode.S)) MovingCamera("down");
+
+        // Scaling camera options
+        if (Input.GetKeyDown(KeyCode.F)) ScalingCamera(true);
+        if (Input.GetKeyDown(KeyCode.G)) ScalingCamera(false);
         
         // Placing tile /w LMB
         if (Input.GetMouseButton(0)) PlaceTile(pos);
@@ -64,6 +75,42 @@ public class LevelEditor : MonoBehaviour
             if (_selectedTileIndex < 0) _selectedTileIndex = LevelManager.instance.tiles.Count - 1;
             Debug.Log($"Selected tile: {LevelManager.instance.tiles[_selectedTileIndex].name}");
         }
+    }
+
+    private void ScalingCamera(bool isScalingUp)
+    {
+        var cameraSize = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().orthographicSize;
+        if (isScalingUp)
+            cameraSize += 2;
+        else
+            cameraSize -= 2;
+        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().orthographicSize = cameraSize;
+    }
+
+    private void MovingCamera(string direction)
+    {
+        var cameraPosition = GameObject.FindGameObjectWithTag("MainCamera").transform.position;
+
+        switch (direction)
+        {
+            case "right":
+                cameraPosition.x += 5;
+                break;
+            case "left":
+                cameraPosition.x -= 5;
+                break;
+            case "up":
+                cameraPosition.y += 5;
+                break;
+            case "down":
+                cameraPosition.y -= 5;
+                break;
+            default:
+                Debug.Log("fuck");
+                break;
+        }
+
+        GameObject.FindGameObjectWithTag("MainCamera").transform.position = cameraPosition;
     }
 
     void PlaceTile(Vector3Int pos)
